ff3e974b93f8fa869f837b46feaaf3daa64bd57a
Luiz Faias <luizfaias@gmail.com>
2014-06-04 21:49:33 -0400

    Making external API call to retrieve user's data

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 (function() {
 
-	return {	
-		events: {
-			'app.activated': 'load_rapportive_info'
-		},
+    return {
+        events: {
+          'app.activated':'load_rapportive_info'
+        },
 
-		load_rapportive_info: function() {
-			var requester_email = this.ticket().requester().email();
-			
-			this.switchTo('load_rapportive_info', {
-				username: requester_email
-			});
-		}
-	};
+        requests: {
+            api_call: function(email) {
+                return {
+                    url: 'http://rapportive-sinatra.herokuapp.com/api/v1/rapportive/' + email,
+                    type:'GET',
+                    dataType: 'json',
+					contentType: 'application/json'
+                };
+            }
+        },
+
+        load_rapportive_info: function() {
+            var requester_email = this.ticket().requester().email();
+            var request = this.ajax('api_call', requester_email);
+            request.done(this.showInfo);
+            request.fail(this.showError);
+        },
+
+        showInfo: function(data) {
+			this.switchTo('rapportive', data);
+        },
+
+        showError: function() {
+            this.switchTo('error');
+        }
+    };
 
 }());
