47c50194a68277a10dff6c1addf822874dda31ce
Luiz Faias <luizfaias@gmail.com>
2014-06-05 10:55:15 -0400

    Modifying API endpoint

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
     return {
         events: {
-          'app.activated':'load_rapportive_info'
+          'app.created':'load_rapportive_info'
         },
 
         requests: {
             api_call: function(email) {
                 return {
-                    url: 'http://rapportive-sinatra.herokuapp.com/api/v1/rapportive/' + email,
+                    url: 'http://rapportive-api-sinatra.herokuapp.com/api/v1/rapportive/' + email,
                     type:'GET',
                     dataType: 'json',
 					contentType: 'application/json'
