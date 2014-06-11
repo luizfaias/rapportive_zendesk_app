53426837666e4168e1824f59f3d3f92f1d57b46e
Luiz Faias <luizfaias@gmail.com>
2014-06-11 10:33:59 -0400

    New feature: update twitter handle from sidebar

--- app.js
+++ app.js
@@ -1,49 +1,65 @@
 (function() {
-	
+
+	var requester_id;
+	var requester_email;
 	var has_twitter_profile;
 
 	return {
 		defaultState: 'loading',
 
 		events: {
-			'app.created': 'init'
+			'app.created': 'init',
+
+			'click .update_twitter_btn': 'update_twitter_handle'
 		},
 
 		requests: {
-			user_identities: function(id) {
+			get_user_identities: function() {
 				return {
-					url: '/api/v2/users/' + id + '/identities.json',
+					url: '/api/v2/users/' + requester_id + '/identities.json',
 					type: 'GET'
 				};
 			},
-			rapportive_api_call: function(email) {
+			rapportive_api_call: function() {
 				return {
-					url: 'http://rapportive-api-sinatra.herokuapp.com/api/v1/rapportive/' + email,
+					url: 'http://rapportive-api-sinatra.herokuapp.com/api/v1/rapportive/' + requester_email,
 					type: 'GET',
 					dataType: 'json',
 					contentType: 'application/json'
 				};
+			},
+			add_user_identity: function(twitter_handle) {
+				return {
+					url: '/api/v2/users/' + requester_id + '/identities.json',
+					type: 'POST',
+					data: {
+						"identity": {
+							"type": "twitter",
+							"value": twitter_handle
+						}
+					}
+				};
 			}
 		},
-		
+
 		init: function() {
+			requester_id = this.ticket().requester().id();
+			requester_email = this.ticket().requester().email();
+
 			this.has_twitter_profile();
 			this.load_rapportive_info();
 		},
 
 		has_twitter_profile: function() {
-			var requester_id = this.ticket().requester().id();				
-			var request = this.ajax('user_identities', requester_id).done(function(data) {
+			var request = this.ajax('get_user_identities', requester_id).done(function(data) {
 				has_twitter_profile = _.pluck(data.identities, "type").contains("twitter");
 			});
 		},
 
 		load_rapportive_info: function() {
-			var requester_email = this.ticket().requester().email();
-
 			if (requester_email == null) {
 				this.switchTo('not_found');
-			} else {			
+			} else {
 				var request = this.ajax('rapportive_api_call', requester_email);
 				request.done(this.render_info);
 				request.fail(this.render_error_page);
@@ -73,6 +89,22 @@
 
 		render_error_page: function() {
 			this.switchTo('error');
+		},
+
+		update_twitter_handle: function() {
+			var twitter_handle = this.$("#twitter_handle").val();
+
+			var request = this.ajax('add_user_identity', twitter_handle);
+
+			request.done(function() {
+				services.notify(this.I18n.t('user.updated'));
+				this.$('.update_twitter_btn').hide()
+			});
+
+			request.fail(function(data) {
+				var error_message = data.responseJSON.details.value[0].description;
+				services.notify(error_message, 'error');
+			});
 		}
 	};
 
